f05d90a910f1599ee204a1c7812423be4db89cca
Dhammikalks <dhammika.lks@gmail.com>
2018-10-18 20:41:29 +0530

    php to support python call to the server

--- simulator/data.php
+++ simulator/data.php
@@ -1,13 +1,27 @@
 <?php //logtime.php
+ob_start('ob_gzhandler');
 header("Access-Control-Allow-Origin: *");
-
-if(isset($_POST['Obstacle']))
+if (isset($_REQUEST['ajaxcallid']))
 {
-    $env = $_POST['Obstacle'];
-    $robot = $_POST['RobotPostion'];
-    $paths = $_POST['Path'];
+    if($_REQUEST['ajaxcallid']==2)
+    {
+      $phparr = $_REQUEST['jsarr'];
+      $tempData = html_entity_decode(stripslashes($phparr));
+      $dphparr = json_decode($tempData);
+      $env = $dphparr->Obstacle;
+     $robot = $dphparr->RobotPostion;
+      $path = $dphparr->Path;
+      $data = [$robot,$path];
 
-$data = [$env,$robot,$paths];
-$result =  shell_exec('./calculate_vectors.py 2>&1  '.escapeshellarg(json_encode($data)));
+      $result =  shell_exec('./index.py 2>&1 '.escapeshellarg(json_encode($data)));
+      #$env_1 = array_slice($env, 150, 50);
+      for ($x = 0; $x < sizeof($env)/50; $x++) {
+          $env_input  = array_slice($env,  $x*50 , 50);
+          $result =  shell_exec('./index.py 2>&1 '.escapeshellarg(json_encode($env_input)));
+        }
+##$result =  shell_exec('./index.py 2>&1 '.escapeshellarg(json_encode($env_1)));
+$result =  shell_exec('./index.py 2>&1 ');
+echo json_encode($result);
+    }
 }
 ?>
